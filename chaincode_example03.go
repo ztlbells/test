17f9af11e12f4179cd7ff7e60aa64d3493bf9a2b
swb <swbsin@163.com>
2016-06-30 16:15:37 +0800

    add test file

--- chaincode_example03.go
+++ chaincode_example03.go
@@ -1,1 +1,134 @@
 
+/*
+auther:swb
+time:16/06/30
+MIT License
+*/
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+// SimpleChaincode example simple Chaincode implementation
+type SimpleChaincode struct {
+}
+
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
+
+// Init resets all the things
+func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	}
+
+	err := stub.PutState("hello_world", []byte(args[0]))
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+// Invoke isur entry point to invoke a chaincode function
+func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	fmt.Println("invoke is running " + function)
+
+  var key, value string
+	var err error
+	
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+
+	key = args[0] //rename for funsies
+	value = args[1]
+	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+	
+	// Handle different functions
+// 	if function == "init" {
+// 		return t.Init(stub, "init", args)
+// 	} else if function == "write" {
+// 		return t.write(stub, args)
+// 	}
+// 	fmt.Println("invoke did not find func: " + function)
+
+// 	return nil, errors.New("Received unknown function invocation")
+}
+
+// Query is our entry point for queries
+func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	fmt.Println("query is running " + function)
+	
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+	
+	var value string
+	var err error
+  
+  err = stub.PutState("hello_world", []byte(value))
+  
+  if err != nil {
+		return nil, err
+	}
+	return nil, nil
+
+	// Handle different functions
+// 	if function == "read" { //read a variable
+// 		return t.read(stub, args)
+// 	}
+// 	fmt.Println("query did not find func: " + function)
+
+// 	return nil, errors.New("Received unknown function query")
+}
+
+// write - invoke function to write key/value pair
+// func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+// 	var key, value string
+// 	var err error
+// 	fmt.Println("running write()")
+
+// 	if len(args) != 2 {
+// 		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+// 	}
+
+// 	key = args[0] //rename for funsies
+// 	value = args[1]
+// 	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return nil, nil
+// }
+
+// read - query function to read key/value pair
+// func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+// 	var key, jsonResp string
+// 	var err error
+
+// 	if len(args) != 1 {
+// 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+// 	}
+
+// 	key = args[0]
+// 	valAsbytes, err := stub.GetState(key)
+// 	if err != nil {
+// 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+// 		return nil, errors.New(jsonResp)
+// 	}
+
+// 	return valAsbytes, nil
+// }
