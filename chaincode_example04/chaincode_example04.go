b0f392915b57d55946bad04f255a17bf29e825fc
swb <swbsin@163.com>
2016-07-07 16:14:04 +0800

    Update chaincode_example04.go

--- chaincode_example04/chaincode_example04.go
+++ chaincode_example04/chaincode_example04.go
@@ -1,1 +1,65 @@
+/*
+	author:swb
+	emial:swbsin@163.com
+	MIT License
+*/
 
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/hex"
+	"io"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+type SimpleChaincode struct {
+}
+
+func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+
+}
+
+func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+
+}
+
+func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+
+}
+
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
+
+//生成Address
+func GetAddress() (string,string,string) {
+	var address,priKey,pubKey string
+	b := make([]byte, 48)
+
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return ""
+	}
+
+	h := md5.New()
+	h.Write([]byte(base64.URLEncoding.EncodeToString(b)))
+
+	address = hex.EncodeToString(h.Sum(nil))
+	priKey = address+"1"
+	pubKey = address+"2"
+	
+	return address,priKey,pubKey
+}
+
+
+func (t *SimpleChaincode) createUser(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+
+}
