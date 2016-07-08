a0e532107cd66e2c147386e03b0845d3f3b22a4e
swb <swbsin@163.com>
2016-07-08 10:07:57 +0800

    Update chaincode_example05.go

--- chaincode_example05/chaincode_example05.go
+++ chaincode_example05/chaincode_example05.go
@@ -21,16 +21,45 @@ import (
 type SimpleChaincode struct {
 }
 
-func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+var homeNo int
 
+type Home struct{
+	Address string
+	Energy int
+	Money int
+	Id int
+	Status int
+	PriKey string
+	PubKey string
+}
+
+func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	if len(args) != 0 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 0")
+	}
+	return nil,nil
 }
 
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-
+	if function == "createUser"{
+		return t.createUser(stub,args)
+	}
+	return nil,errors.New("Received unknown function invocation")
 }
 
 func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-
+	if function == "getHomeByAddress"{
+		if len(args) != 1{
+			return nil, errors.New("Incorrect number of arguments. Expecting 1")
+		}
+		_,homeBytes,err = getHomeByAddress(stub,args[0])
+		if err != nil {
+				fmt.Println("Error get home")
+				return nil, err
+			}
+		return homeBytes,nil
+	}
+	return nil,errors.New("Received unknown function invocation")
 }
 
 func main() {
@@ -40,12 +69,13 @@ func main() {
 	}
 }
 
+//生成Address
 func GetAddress() (string,string,string) {
 	var address,priKey,pubKey string
 	b := make([]byte, 48)
 
 	if _, err := io.ReadFull(rand.Reader, b); err != nil {
-		return ""
+		return "","",""
 	}
 
 	h := md5.New()
@@ -54,14 +84,64 @@ func GetAddress() (string,string,string) {
 	address = hex.EncodeToString(h.Sum(nil))
 	priKey = address+"1"
 	pubKey = address+"2"
-	
+
 	return address,priKey,pubKey
 }
 
-func (t *SimpleChaincode) createSchool(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-	
+
+func (t *SimpleChaincode) createUser(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var energy , money int
+	var err error
+	var homeBytes []byte
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2")
+	}
+	address,priKey,pubKey := GetAddress()
+	energy ,err = strconv.Atoi(args[0])
+	if err !=nil{
+		return nil,errors.New("want Integer number")
+	}
+	money , err = strconv.Atoi(args[1])
+	if err != nil{
+		return nil,errors.New("want Integer number")
+	}
+	home :=Home{Address:address,Energy:energy,Money:money,Id:homeNo,Status:1,PriKey:priKey,PubKey:pubKey}
+	err = writeHome(stub,home)
+	if err != nil {
+		return nil, errors.New("write Error" + err.Error())
+	}
+	homeBytes,err = json.Marshal(&home)
+	if err!= nil{
+		return nil,errors.New("Error retrieve")
+	}
+	return homeBytes, nil
 }
 
-func (t *SimpleChaincode) createStudent(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+func writeHome(stub *shim.ChaincodeStub,home Home) (error) {
+	homeBytes, err := json.Marshal(&home)
+	if err != nil {
+		return err
+	}
+	err = stub.PutState(home.Address, homeBytes)
+	if err != nil {
+		return errors.New("PutState Error" + err.Error())
+	}
+	return nil
+}
+
+func getHomeByAddress(stub *shim.ChaincodeStub, address string) (Home,[]byte, error) {
+	var home Home
+	homeBytes,err := stub.GetState(address)
+	if err != nil {
+		fmt.Println("Error retrieving home")
+	}
+	err = json.Unmarshal(homeBytes, &home)
+	if err != nil {
+		fmt.Println("Error unmarshalling home")
+	}
+	return home,homeBytes, nil
+}
+
+func getHomesByEnergyline(stub *shim.ChaincodeStub, address string)([]Home,[]byte,error){
 	
 }
